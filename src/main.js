f27c131d0cd1883627ffaece88028da54471ffd8
Yoo <santiagofrieza@gmail.com>
2023-03-02 18:37:18 -0500

    Container function added (Fix the taildwind classes support here)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import NavBar from "./UI/NavBar";
 
+function Container() {
+    const Container = document.createElement("div");
+    Container.classList.add("bg-zinc-100");
+    return Container
+}
+
 document.body.appendChild(NavBar());
+document.body.appendChild(Container());
