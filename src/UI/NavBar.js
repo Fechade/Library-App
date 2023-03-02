584d779e41078b6ddaf9ff554ab196e1dd3629ff
Yoo <santiagofrieza@gmail.com>
2023-03-02 18:37:56 -0500

    Split the Style functions in a different file

--- src/UI/NavBar.js
+++ src/UI/NavBar.js
@@ -2,12 +2,16 @@ import { SignIn, SignOut, auth } from "../firebase-app";
 import { onAuthStateChanged } from "firebase/auth";
 function NavBar() {
   const navBar = document.createElement("nav");
+  NavBarStyles(navBar);
 
   const Logo = document.createElement("h1");
+  LogoStyles(Logo);
   Logo.textContent = "LOGO";
 
   const Login = document.createElement("button");
+  SignStyles(Login)
   const LogOut = document.createElement("button");
+  SignStyles(LogOut)
 
   navBar.appendChild(Logo);
   navBar.appendChild(Login);
@@ -35,5 +39,33 @@ function NavBar() {
 
   return navBar;
 }
+
+function NavBarStyles(navBar) {
+  navBar.classList.add("bg-white");
+  navBar.classList.add("text-black");
+  navBar.classList.add("p-6");
+  navBar.classList.add("pl-20");
+  navBar.classList.add("pr-20");
+  navBar.classList.add("flex");
+  navBar.classList.add("justify-between");
+  navBar.classList.add("text-lg");
+  navBar.classList.add("items-center");
+  navBar.classList.add("drop-shadow-lg");
+}
+
+function SignStyles(btn) {
+  btn.classList.add("bg-slate-50");
+  btn.classList.add("p-3");
+  btn.classList.add("pl-4");
+  btn.classList.add("pr-4");
+  btn.classList.add("rounded-lg");
+  btn.classList.add("font-semibold");
+  btn.classList.add("text-base");
+}
+
+function LogoStyles(logo) {
+  logo.classList.add("font-bold")
+  logo.classList.add("text-4xl")
+} 
 
 export default NavBar;
